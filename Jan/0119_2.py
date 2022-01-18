55be7045cc0771064f23e614f8096fa6f3a01b20
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-01-19 00:26:08 +0900

    0119 끝!

--- Jan/0119_2.py
+++ Jan/0119_2.py
@@ -1,6 +1,6 @@
 N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
-for i in A:
-    if i < X:
-        print(i, end=' ')
+for i in A: # A에 들어있는 값 중
+    if i < X: # X보다 작은 값이 있으면
+        print(i, end=' ') # 출력하라
---
+++ Jan/0119_3.py
@@ -0,0 +1,4 @@
+# (R1+R2)/2 = S
+# => R2 = 2S-R1
+R1, S = map(int, input().split())
+print(2*S - R1)
