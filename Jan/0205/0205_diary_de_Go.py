8a7e5b9e4486dc0b8d93bff01f488829f97134b5
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-01-29 18:04:28 +0900

    diary_de_Go

--- Jan/0205/0205_diary_de_Go.py
+++ Jan/0205/0205_diary_de_Go.py
@@ -1,3 +1,5 @@
+# 남들이 자신의 일기장을 보는 것을 막기 위해 모음 다음에 'p'를 하나 쓰고, 그 모음을 하나 더 쓴다.
+# 원래 문장은 무엇인지 구하는 프로그램을 작성하시오.
 
 diary = input()
 vowl = ['a', 'i', 'o', 'u', 'e']
@@ -20,14 +22,40 @@ new_diary = ''
 #         # len -= 2
 #     i += 1
 
-two, one = '', ''
+
+# two, one = '', ''
+# for chr in diary:
+#     if two == chr in vowl and one == 'p':
+#         two, one = '', ''
+#     else:
+#         new_diary += two
+#     two, one = one, chr
+# new_diary += two + one
+# 얘는 epe를 암호화했을 때 나오는 epepepe에 취약함
+
+
+# [ ][ ][ ]
+# zepelepenapa papapripikapa
+# [ ][ ][z]epelepenapa papapripikapa
+# [ ][z][e]pelepenapa papapripikapa
+# [z][e][p]elepenapa papapripikapa
+# ...
+# [e][l][e]penapa papapripikapa
+# [l][e][p]enapa papapripikapa
+# [e][p][e]napa papapripikapa
+# [e][ ][ ]napa papapripikapa
+# ...
+# [k][a][p]a
+# [a][p][a]
+
+two, one, this = '', '', '' # 문자열을 읽어들어가면서 뒤에 있는 두 번째까지 
 for chr in diary:
+    two, one, this = one, this, chr
+    new_diary += two
     if two == chr in vowl and one == 'p':
-        two, one = '', ''
-    else:
-        new_diary += two
-    two, one = one, chr
-new_diary += two + one
+        this, one = '', ''
+    # two, one = one, chr
+new_diary += one + this
 
 
 print(new_diary)
