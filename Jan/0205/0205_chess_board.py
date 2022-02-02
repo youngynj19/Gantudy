4161caca8a3f96e15c9886da49c6433ba1902b60
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-02-02 22:22:44 +0900

    chess modifying

--- Jan/0205/0205_chess_board.py
+++ Jan/0205/0205_chess_board.py
@@ -1,26 +1,19 @@
-# 체스판
+# # 체스판
 
-# R: 세로 총 칸 수
-# C: 가로 ~
-# A: 칸 높이
-# B: 칸 너비
+# # R: 세로 총 칸 수
+# # C: 가로 ~
+# # A: 칸 높이
+# # B: 칸 너비
 
-# XX..XX..
-# XX..XX..
-# ..XX..XX
-# ..XX..XX
+# # XX..XX..
+# # XX..XX..
+# # ..XX..XX
+# # ..XX..XX
 
+# 밑에 함수 있음!!!!!
 
 R, C = map(int, input().split())
 A, B = map(int, input().split())
-
-
-# 이중 for문 말고 다른 방법... 전에 썼던 그 방법 되는지 해보기! ㅅㅂ.........
-# print(('*' * n + '\n') * m).... ㅈ되네
-# C가 2라서 range(0이면??????) - 홀짝 나눠서 해결
-# 음..... 재귀함수?
-
-
 
 if R%2: # 홀수번 밑으로 갈 땐 # .......... (4)세로
 
@@ -79,12 +72,39 @@ else: # ............... (5) 세로
                     print(f'{"."*B}{"X"*B}', end='')
                 print(f'{"."*B}{"X"*B}')
 
-    # for j in range(A): # ............ (3) 세로
-    #         if C%2: # 홀수번 옆으로 갈 땐
-    #             for i in range(C//2): 
-    #                 print(f'{"X"*B}{"."*B}', end='') 
-    #             print('X'*B)
-    #         else: # 짝수번 옆으로 갈 때
-    #             for i in range(C//2 -1):
-    #                 print(f'{"X"*B}{"."*B}', end='')
-    #             print(f'{"X"*B}{"."*B}')
+
+
+
+
+
+
+
+
+
+
+
+def chess(length, width, height, black, white):
+    for j in range(height):
+        if length%2: # 홀수번 옆으로 갈 땐 ......... 가로(4)
+            for i in range(length//2): # .......... 가로(2)
+                print(f'{black*width}{white*width}', end='') # 경험에 의한 결과^^;; ........... 가로(1)
+            print(black*width) # ............ 가로(3)
+        else: # 짝수번 옆으로 갈 때 ............ 가로(5)
+            for i in range(length//2 -1):
+                print(f'{black*width}{white*width}', end='')
+            print(f'{black*width}{white*width}')
+
+R, C = map(int, input().split())
+A, B = map(int, input().split())
+
+if R%2: # 홀수번 밑으로 갈 땐 # .......... (4)세로
+
+    for k in range(R//2): # .......... (2)세로
+        chess(C,B,A,'X','.')
+        chess(C,B,A,'.','X')
+    chess(C,B,A,'X','.')
+
+else: # ............... (5) 세로
+    for k in range(R//2): # .......... (2)세로
+        chess(C,B,A,'X','.')
+        chess(C,B,A,'.','X')
--- Jan/0205/0205_gcd_lcm.py
+++ Jan/0205/0205_gcd_lcm.py
@@ -1,43 +1,36 @@
+# 두 개의 자연수를 입력받아 최대 공약수와 최소 공배수를 출력하는 프로그램을 작성하시오.
 
 # A, B = map(int, input().split())
 
-# # 최소공배수는 ..... set으로 풀면... 안될 것 같고....
-# # 최대공약수는.... 약수 찾아서
-# # 최댓값?
-# # ㅅㅂ 개어렵네
-
 # A_list, B_list = [], [] # 일단 A, B 모든 약수 모아놓을 리스트
-# ########### 인데 중복되는 약수느 ㄴ어떻게?????
 
-# for i in range(1,A+1):
+# for i in range(1,A+1): # A의 약수면 리스트에 넣기
 #     if not A%i:
 #         A_list += [i]
 
 # for i in range(1,B+1):
 #     if not B%i:
 #         B_list += [i]
-# print(A_list)
-# print(B_list)
-# # 리스트 둘 다 set으로 묶어서 공집합? 하면 최대공약수는 구할 듯
+# print(A_list) # 답 안나와서 일단 프린트라도 해봄
+# print(B_list) # 근데 해보니까 set으로 묶으면 될 것 같길래 일단 해봄
 
-# A_and_B = set(A_list) & set(B_list)
-# # A_or_B = set(A_list) | set(B_list)
+# A_and_B = set(A_list) & set(B_list) # 해보니까 공약수들만 딱 모이길래 최대값 구함
 
 # print(max(A_and_B))
 # # print(A_or_B)
 
-# AB_list = [] # A*B의 모든 약수
-# for i in range(1,A*B+1):
+# AB_list = [] # A*B의 모든 약수 # 공배수는 저 방식으로 안풀려서 거시적으로 봄
+# for i in range(1,A*B+1): # A*B의 약수면 리스트에 넣고
 #     if not (A*B)%i:
 #         AB_list += [i]
 
-# AB_bigger = [] # A와 B의 공배수
+# AB_bigger = [] # A와 B의 공배수 * 공배수를 구하는 쪽으로
 # for num in AB_list:
 #     if not (num%A or num%B): # 둘 중 하나라도 1이면 괄호가 1 -> 0
 #         AB_bigger += [num]
 # print(min(AB_bigger))
 
-# ########### set에서 제일 큰 애가 최대 공약수인가????? 맞음
+# ########### set에서 제일 큰 애가 최대 공약수인가????? 맞음 밑에서 확인
 
 # # a^3, b^3, c^3
 # # a^2, b^5, d^1
@@ -98,23 +91,26 @@
 
 
 
-# A, B = map(int, input().split())
-
-# smallest = 0
-# biggest = 0 
- 
-# for i in range(1,A*B+1):
-#     if not (A%i or B%i):
-#         smallest = i
-#     elif not (i%A or i%B):
-#         biggest = i
-#         break
-        
-# print(smallest)
-# print(biggest)
-
-import math
 A, B = map(int, input().split())
 
-print(math.gcd(A,B))
-print(math.lcm(A,B))
+smallest = 0
+biggest = 0 
+ 
+for i in range(1,A*B+1): # 공배수, 공약수 다 나오는 리스트
+    if not (A%i or B%i): # 에서 리스트도 빼고 공약수들만 골라내서 제일 마지막에 잡히는 애 넣음
+        smallest = i
+    elif not (i%A or i%B): # 그 후에는 공배수중에서 제일 먼저 잡히는 애만 잡고 break
+        biggest = i
+        break
+        
+print(smallest)
+print(biggest)
+
+
+# ^^ 해답지
+
+# import math
+# A, B = map(int, input().split())
+
+# print(math.gcd(A,B))
+# print(math.lcm(A,B))
