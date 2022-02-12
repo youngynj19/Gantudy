5da2cc5ad5403cb87ce96f7af92d09312199fd2b
dbsdudwls19 <dbsdudwls19@gmail.com>
2022-02-12 16:41:38 +0900

    blackjack continue...

--- Feb/0215/0210_blackjack.py
+++ Feb/0215/0210_blackjack.py
@@ -17,6 +17,10 @@
 
 N,M = map(int, input().split())
 cards = list(map(int, input().split()))
+
+
+
+
 # cards.sort()
 
 # for문에서 하나 빼놓고
@@ -24,26 +28,26 @@ cards = list(map(int, input().split()))
 # while문 나와서 for문 안에서 이전 card랑 비교하기
 # for 다 돌고 가장 작은 거 나오면 for 나와서 출력
 
-min = 300000
-result = 0
-# for card in cards:
-#     new_cards = cards[:]
-#     new_cards.remove(card)
+# min = 300000
+# result = 0
+# for cards[i] in cards:
+#     cards = cards[:]
+#     cards.remove(cards[i])
     
 #     s=0
-#     e=len(new_cards)-1 # 투포인터...
+#     e=len(cards)-1 # 투포인터...
 #     temp_min = 300000 # M과의 차를 담을 변수, 갈수록 작아짐
 #     temp_result = 0
 #     while s < e:
-#         temp_diff = abs(new_cards[s]+new_cards[e]+card - M)
+#         temp_diff = abs(cards[s]+cards[e]+cards[i] - M)
 #         if temp_diff < temp_min:
 #             temp_min = temp_diff
-#             temp_result = new_cards[s] + new_cards[e] + card
+#             temp_result = cards[s] + cards[e] + cards[i]
             
 #         if temp_min == 0:
 #             break
         
-#         if new_cards[s]+new_cards[e]-M > 0:
+#         if cards[s]+cards[e]-M > 0:
 #             e -= 1
 #         else:
 #             s += 1
@@ -54,19 +58,66 @@ result = 0
     
 #     if min == 0:
 #         break
+# print(result)
 
 
 
-for i in range(N):
-    for j in range(N):
+
+
+
+# cards.sort()
+
+# min = 300000
+# result = 0
+# for i in range(N):
+
+#     s=0
+#     e=len(cards) # 투포인터...
+#     temp_min = 300000 # M과의 차를 담을 변수, 갈수록 작아짐
+#     # while문에서 나온 result(i가 변하면 얘도 바뀔 수 있음)
+#     temp_result = 0
+#     while s < e:
+#         # 0보다 커야하는 값, M과의 차이
+#         temp_diff = M - cards[s]+cards[e]+cards[i]
+
+#         # 근데 M보다 합이 크면
+#         if temp_diff < 0:
+#             e -= 1
+#         else: # 지금 차이값이 이전 차이값보다 작으면 없뎃
+#             if temp_diff < temp_min:
+#                 temp_min = temp_diff
+#                 temp_result = cards[s] + cards[e] + cards[i]
+#             # 혹시 0일 땐 더 찾아볼 필요 없음    
+#             if temp_min == 0:
+#                 break
+#             s += 1
+
+#     if temp_min < min:
+#         min = temp_min
+#         result = temp_result
+    
+#     if min == 0:
+#         break
+# print(result)
+
+
+
+
+
+
+min = 300000
+result = 0
+
+for i in range(N-2):
+    for j in range(i+1, N-1):
         if j == i:
             continue
-        for k in range(N):
+        for k in range(j+1, N):
             if k == j or k == i:
                 continue
             temp_result = cards[i]+cards[j]+cards[k]
-            temp_min = abs(temp_result - M)
-            if temp_min <= min:
+            temp_min = M - temp_result
+            if 0 < temp_min and temp_min <= min:
                 min = temp_min
                 result = temp_result
 print(result)
